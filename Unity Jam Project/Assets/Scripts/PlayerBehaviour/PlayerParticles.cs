792b5977eb9d73c151941a429ba0a7a6c21e66b4
Alejandro Campbell <alejandro.campbell@live.u-tad.com>
2025-06-26 18:23:37 +0200

    Basic movement and player logic prototyped

---
+++ Unity Jam Project/Assets/Scripts/PlayerBehaviour/PlayerParticles.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using GeneralPurpose;
+using UnityEngine;
+
+namespace PlayerBehaviour
+{
+    [RequireComponent(typeof(Player))]
+    public class PlayerParticles : ParticlePlayer
+    {
+        protected Player m_player;
+
+        [Header("Particles")] public ParticleSystem turnParticle;
+        
+        
+        public override void InitializeEvents()
+        {
+            m_player.OnPlayerTurn.AddListener(PlayTurnParticle);
+        }
+
+        protected void PlayTurnParticle()
+        {
+            Play(turnParticle);
+        }
+        
+        protected void Awake()
+        {
+            m_player = GetComponent<Player>();
+        }
+    }
+}
